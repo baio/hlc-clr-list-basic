5e1f637f29ead4e2fba33860197aeea69e61c50d
baio <max.putilov@gmail.com>
2019-07-29 22:51:24 +0300

    Initial commit

---
+++ src/app/table.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Table } from '@ng-holistic/clr-list';
+import { Subject, timer } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+
+// Provide table UI definition in js object
+const definition: Table.Definition = {
+    cols: [
+        {
+            id: 'title',
+            title: 'Title',
+            sort: true
+        },
+        {
+            id: 'amount',
+            title: 'Amount',
+            sort: false
+        }
+    ]
+};
+
+// Provide data for the table
+const rows: Table.Row[] = [
+    {
+        id: '1',
+        title: 'aaaa',
+        amount: 2
+    },
+    {
+        id: '2',
+        title: 'bbb',
+        amount: 3
+    }
+];
+
+const dataProvider: Table.Data.DataProvider = {
+    load(_) {
+        return timer(0).pipe(mapTo({ rows }));
+    }
+};
+
+@Component({
+  selector: 'my-table',
+  template: '<hlc-clr-table [definition]="definition" [dataProvider]="dataProvider"></hlc-clr-table>'
+})
+export class TableComponent  {
+  definition = definition;
+  dataProvider = dataProvider;
+}
